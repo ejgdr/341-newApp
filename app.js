e5fc1c0641ea9427554be9db9dcb0a73fdf14f5b
ejgdr <gui20006@byui.edu>
2022-06-04 18:31:50 -0600

    make it better

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 // To check that is working 
 app.get('/', (req, res, next) => {
-    res.send('Hello world!');
+    res.send('Hello world again!');
 })
 
 app.listen(3000);
