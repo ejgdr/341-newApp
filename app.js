1f3701eb84a1f87a3bed82c9d2a0b2d6652063d1
ejgdr <gui20006@byui.edu>
2022-06-04 21:15:55 -0600

    Graphql responding to a simple string

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-// To check that is working 
-app.get('/', (req, res, next) => {
-    res.send('Hello world again!');
-})
+// Check my graphql documentation
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: buildSchema(`
+            type RootQuery {
+                events: [String!]!
+            }
 
-app.listen(3000);
+            type RootMutation {
+                createEvent(name: String): String
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            events: () => {
+                return ['Grade 9', 'Grade 10', 'Grade 11'];
+            },
+            createEvent: (args) => {
+                const eventName =  args.name;
+                return eventName;
+            }
+        }, //has all the resolver functions and need to match the end point of our schema by name
+        graphiql: true 
+    })
+);
+
+app.listen(4000);
