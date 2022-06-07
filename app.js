51f82726105f95d38442fab8b53f750ae224be99
ejgdr <gui20006@byui.edu>
2022-06-07 16:33:25 -0600

    data simulating memory, connection mongodb

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 
 const app = express();
+
+const events = [];
 
 app.use(bodyParser.json());
 
@@ -13,12 +16,27 @@ app.use(
     '/graphql',
     graphqlHTTP({
         schema: buildSchema(`
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
             type RootQuery {
-                events: [String!]!
+                events: [Event!]!
             }
 
             type RootMutation {
-                createEvent(name: String): String
+                createEvent(eventInput: EventInput): Event
             }
 
             schema {
@@ -28,17 +46,29 @@ app.use(
         `),
         rootValue: {
             events: () => {
-                return ['Grade 9', 'Grade 10', 'Grade 11'];
+                return events;
             },
             createEvent: (args) => {
-                const eventName =  args.name;
-                return eventName;
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: args.eventInput.date
+                };
+                events.push(event);
+                return event;
             }
         }, //has all the resolver functions and need to match the end point of our schema by name
         graphiql: true 
     })
 );
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-  });
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustercse341.8zfoc.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`);
+      });
+}).catch(err => {
+    console.log(err);
+});
+
