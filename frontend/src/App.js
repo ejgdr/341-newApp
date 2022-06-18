97b0f8bc58ee8862b6bb69acb2bec56b016b23c5
ejgdr <gui20006@byui.edu>
2022-06-18 00:19:17 -0600

    Revert "navigation bar"
    This reverts commit 2f34d9a32c55b274fc6c862425b9a36e2191cf85.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,23 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import TrackingsPage from './pages/Trackings';
-import MainNavigation from './components/Navigation/MainNavigation';
-import React from 'react';
 
 function App() {
   return (
     <BrowserRouter>
-      <React.Fragment>
-        <MainNavigation/>
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<AuthPage/>} exact/>
-              <Route path="/auth" element={<AuthPage/>}/>
-              <Route path="/events" element={<EventsPage/>}/>
-              <Route path="/trackings" element={<TrackingsPage/>}/>
-          </Routes>
-        </main>
-      </React.Fragment>
+      <Routes>
+        <Route path="/" element={<AuthPage/>} exact/>
+          <Route path="/auth" element={<AuthPage/>}/>
+          <Route path="/events" element={<EventsPage/>}/>
+          <Route path="/trackings" element={<TrackingsPage/>}/>
+      </Routes>
     </BrowserRouter>
   );
 }
