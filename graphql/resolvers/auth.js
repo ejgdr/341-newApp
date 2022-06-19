cf0c471f3872b8a76f9d032128b6334e4662e315
ejgdr <gui20006@byui.edu>
2022-06-18 23:41:58 -0600

    send

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -38,7 +38,7 @@ module.exports = {
         if (!isEqual) {
             throw new Error('Password is incorrect!');
         }
-        const token = jwt.sign({userId: user.id, email: user.email}, process.env.SECRET, {expiresIn: '1h'});
+        const token = jwt.sign({userId: user.id, email: user.email}, 'somesupersecretkey', {expiresIn: '1h'});
         return { userId: user.id, token: token, tokenExpiration: 1 };
     }
 };
